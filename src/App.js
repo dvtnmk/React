48b311d12b3c63f15f2acd8bf5f215717097e4f6
DvtNmk <devotion.nk@gmail.com>
2019-01-19 14:20:00 +0700

    Set State and React Life cycle

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { FormGroup, FormControl, Label } from "react-bootstrap";
 let object = {
   width: 200,
   height: 500,
-  backgroundColor: "red"
+  margin: 10
 };
 
 const onClick = function() {
@@ -15,33 +15,68 @@ const onEnter = function() {
   alert("Hello Enter");
 };
 
-let forms = [
-  {
-    label: "Email",
-    input: { type: "email", value: "", placeholder: "Enter an Email" }
-  },
-  {
-    label: "First name",
-    input: { value: "", placeholder: "Enter an first name" }
-  },
-  {
-    label: "Last name",
-    input: { value: "", placeholder: "Enter an last name" }
-  },
-  {
-    label: "Phone number",
-    input: { onClick: onEnter, value: "", placeholder: "Enter an number" }
-  }
-];
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(props.test); initial ptop
+    this.state = {
+      name: props.name,
+      forms: [
+        {
+          label: "Email",
+          input: { type: "email", value: "", placeholder: "Enter an Email" }
+        },
+        {
+          label: "First name",
+          input: { value: "", placeholder: "Enter an first name" }
+        },
+        {
+          label: "Last name",
+          input: { value: "", placeholder: "Enter an last name" }
+        },
+        {
+          label: "Phone number",
+          input: { onClick: onEnter, value: "", placeholder: "Enter an number" }
+        }
+      ]
+    };
+  }
+  // componentWillMount
+  onChange = e => {
+    let value = e.target.value;
+    this.setState({
+      name: value
+    });
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Update name state in Should Update", this.state.name);
+    console.log("Next name state in Should Update", nextState);
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log("Update name state in Should Update", this.state.name);
+    console.log("Next name state in Should Update", this.state.name);
+    return true;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Update State", this.state.name);
+    console.log("Update State", this.state.name);
+  }
   render() {
+    console.log("Render State", this.state.name);
+
     return (
       <FormGroup className="App" style={object}>
-        {forms.map(function(forms) {
+        {this.state.forms.map((forms, index) => {
           return (
-            <React.Fragment>
-              <label>{forms.label}</label>
-              <FormControl {...forms.input} />
+            <React.Fragment key={index}>
+              <Label>{forms.label + " " + this.state.name} </Label>
+              <FormControl
+                {...forms.input}
+                value={this.state.name}
+                onChange={this.onChange}
+              />
             </React.Fragment>
           );
         })}
