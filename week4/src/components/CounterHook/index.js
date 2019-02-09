5506ddb77237ad7c071ab37b180ab74b431aecb4
DvtNmk <devotion.nk@gmail.com>
2019-02-09 11:43:38 +0700

    [Add]Compare Hook Life cycle

--- week4/src/components/CounterHook/index.js
+++ week4/src/components/CounterHook/index.js
@@ -1,17 +1,36 @@
 import React, { useEffect, useState } from "react";
 
 const initial = 0;
-
-export function useCount() {
-  const [count, setCount] = useState(initial);
-  return [count, setCount];
+// store.set(() => {}) //call as function
+// store.set(0);//not call function
+export function useCount(initial) {
+  return useState(initial);
 }
 
 export const increseCount = setter => () => {
   setter(count => count + 1);
 };
+
+const ComponentDidMount = () => {
+  console.log("ComponentDidMount");
+};
+
+const componentWillUnMount = () => {
+  console.log("componentWillUnMount");
+};
+
+const componentDidUpdate = () => {
+  console.log("componentDidUpdate");
+};
 function CouterHook() {
   const [count, setCount] = useCount(initial);
+  useEffect(() => {
+    if (1 < 0) {
+      ComponentDidMount();
+    }
+    return componentWillUnMount;
+  }, []);
+  useEffect(componentDidUpdate, [count]);
   return (
     <div>
       <span>{count}</span>
--- week4/src/containers/AppContainer/index.js
+++ week4/src/containers/AppContainer/index.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import Counter from "../../components/Counter";
 import CounterHook from "../../components/CounterHook";
 class AppContainer extends Component {
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({ hide: true });
+    }, 2000);
+  };
   render() {
     return (
       <div>
