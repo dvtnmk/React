772031236ec5d5bf324286c78e42662f936e4067
DvtNmk <devotion.nk@gmail.com>
2019-02-23 09:29:52 +0700

    [Add]Style to storybook

--- week5/src/containers/SignIn/index.js
+++ week5/src/containers/SignIn/index.js
@@ -1,25 +1,55 @@
 import React, { useEffect, useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
+import { API } from "../../configs";
+import "./styles.css";
 
-function onSigin() {}
-
+function onSigin(username, password) {
+  return async function(e) {
+    e.preventDefault();
+    try {
+      const result = await API.fetchAPI(
+        `/users?username=${username}&password=${password}`
+      );
+      console.log(result);
+      if (result.length > 0) {
+        alert("LogedIn :)");
+      } else {
+        alert("Username or password incorrect!!!");
+      }
+    } catch (err) {
+      if (err.hasOwnProperty("response")) {
+        alert(err.response.error.message);
+      }
+    }
+  };
+}
 function SignIn() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <div>
-      <form onSubmit={onSigin}>
-        <Input value={username} placeholder="Username" label="Username" />
+    <div className="login">
+      <form onSubmit={onSigin(username, password)}>
+        <Input
+          value={username}
+          placeholder="Username"
+          label="Username"
+          onChange={e => setUsername(e.target.value)}
+        />
         <Input
           type="password"
           value={password}
           placeholder="Password"
+          onChange={e => setPassword(e.target.value)}
           label="Password"
         />
-        <Button type="submit">SignIn</Button>
-        <Button type="button">SignUp</Button>
+        <Button color="primary" type="submit">
+          SignIn
+        </Button>
+        <Button color="primary" type="button">
+          SignUp
+        </Button>
       </form>
     </div>
   );
